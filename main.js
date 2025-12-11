cf79534694b72518d66cf31ce7927e9a79c94781
Kali-0007 <srivastavaravindra18@gmail.com>
2025-12-11 17:08:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Home page loaded");
+<script>
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault(); // stops page reload
+
+    const box = document.getElementById("successBox");
+    box.style.display = "block";
+
+    // auto-hide after 4 seconds
+    setTimeout(() => {
+        box.style.display = "none";
+    }, 4000);
+});
+</script>
