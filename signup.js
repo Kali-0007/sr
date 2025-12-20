34cfdbea788cc3530cd1589c74222d70711a5b92
Kali-0007 <srivastavaravindra18@gmail.com>
2025-12-20 09:02:58 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx5fxHQmwiKjmkp3tf-yW6V9xCfOdmivGaCWKd3Vu6dJc91Q7LSdM39AIl5-1YK8yB9/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyyeu_P20gJnJowqNCtEoMZ8fdtWzMO1q1vCakQtGuFdGzu420wI3eUfdkrJNwsNVug/exec";
 
 let emailForOtp = null;
 
