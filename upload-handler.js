a2e90ee34f907f90e4b07077136015ab0cbd3f0a
Kali-0007 <srivastavaravindra18@gmail.com>
2025-12-20 10:44:05 +0530

    Update upload-handler.js

--- upload-handler.js
+++ upload-handler.js
@@ -3,7 +3,7 @@
  */
 
 // Updated API URL - Single endpoint for all actions (CORS fixed)
-const API_URL = 'https://script.google.com/macros/s/AKfycbzJfgxKGMWmuZ-dNswmjuiNHLo62NlL9cPe7PtUc4dngnPI1TgR-B6mCmRCs9WYScfI/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzd81RjjizVxsTxSgXK8ur9ge_nzui1iDd-y6HnpZE6xEqc89a9qr8ihWdFdpEldKwK/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
     initializeUploadHandler();
